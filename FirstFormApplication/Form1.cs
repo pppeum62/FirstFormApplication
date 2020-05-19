01fe994531afaa77cb2f670f64d4a479ad974396
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-19 21:23:34 +0900

    코드에서 요소 생성

--- FirstFormApplication/Form1.cs
+++ FirstFormApplication/Form1.cs
@@ -15,6 +15,25 @@ namespace FirstFormApplication
         public Form1()
         {
             InitializeComponent();
+
+            myButton.Enabled = true;
+            myButton.Text = "코드에서 변경";
+            myButton.Width = 200;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Button button = new Button();
+                Controls.Add(button);
+                button.Location = new Point(13, 30 + (23 + 3) * i );
+                button.Text = "동적 생성 " + i + "번 버튼";
+                button.Width = 150;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
